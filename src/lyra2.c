c7862b5599b7133453b48673db550f99c1352bd5
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2014-09-09 11:50:58 -0300

    Finished setup phase.

--- src/lyra2.c
+++ src/lyra2.c
@@ -5,6 +5,10 @@
 #include <immintrin.h>
 #include <assert.h>
 
+static const size_t b = SPONGE_EXTENDED_RATE_SIZE_BYTES;
+static const size_t b128 = SPONGE_EXTENDED_RATE_LENGTH_I128;
+typedef __m128i block_t[b128];
+
 static inline void
 write_basil(uint8_t *buf, unsigned int keylen, const char *pwd,
             unsigned int pwdlen, const char *salt, unsigned int saltlen,
@@ -38,8 +42,10 @@ lyra2(char *key, unsigned int keylen, const char *pwd, unsigned int pwdlen,
       const char *salt, unsigned int saltlen, unsigned int R, unsigned int C,
       unsigned int T) {
 
+    sponge_t *sponge = sponge_new();
+
     size_t basil_size = pwdlen + saltlen + 6 * sizeof(int);
-    size_t row_length_i128 = SPONGE_EXTENDED_RATE_LENGTH_I128 * C;
+    size_t row_length_i128 = b128 * C;
     size_t matrix_size = R * row_length_i128 * sizeof(__m128i);
     if (matrix_size < basil_size + SPONGE_RATE_SIZE_BYTES) {
         // can't fit the basil inside the matrix for the initial
@@ -50,10 +56,26 @@ lyra2(char *key, unsigned int keylen, const char *pwd, unsigned int pwdlen,
     __m128i (*matrix)[row_length_i128] = malloc(R * sizeof(*matrix));
     assert(matrix_size == R * sizeof(*matrix));
 
-    sponge_t *sponge = sponge_new();
-
     write_basil((uint8_t *) matrix, keylen, pwd, pwdlen, salt, saltlen, R, C, T);
     sponge_absorb(sponge, (uint8_t *) matrix, basil_size, 0);
+
+    for (unsigned int col = 0; col < C; col++) {
+        int flags = 0;
+        flags |= SPONGE_FLAG_REDUCED;
+        flags |= SPONGE_FLAG_EXTENDED_RATE;
+        flags |= SPONGE_FLAG_ASSUME_PADDING;
+        sponge_squeeze(sponge, (uint8_t *) &matrix[0][(C-1-col)*b128], b, flags);
+    }
+
+    for (unsigned int col = 0; col < C; col++) {
+        sponge_reduced_extended_duplexing(sponge,
+            (const uint8_t *) &matrix[0][col*b128],
+            (uint8_t *) &matrix[1][(C-1-col)*b128]);
+
+        for (unsigned int i = 0; i < b128; i++) {
+            matrix[1][(C-1-col)*b128+i] ^= matrix[0][col*b128+i];
+        }
+    }
 
     key++; // avoid warning for now
     free(matrix);
