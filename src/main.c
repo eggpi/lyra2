80eaf4d7848cba482381ccb06dc5074ec76bb246
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2014-09-11 15:32:46 -0300

    Added code for some simple time measurements to main.c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include <string.h>
+#include <sys/time.h>
 
 int
 main(void) {
@@ -9,7 +10,16 @@ main(void) {
     char *pwd = "Lyra sponge";
     char *salt = "saltsaltsaltsalt";
 
+	struct timeval t0;
+	struct timeval t1;
+	gettimeofday(&t0, 0);
+
     lyra2(key, sizeof(key), pwd, strlen(pwd), salt, strlen(salt), 10, 64, 10);
+
+	gettimeofday(&t1, 0);
+	unsigned long elapsed = (t1.tv_sec-t0.tv_sec) * 1000000 + t1.tv_usec-t0.tv_usec;
+	printf("Execution Time: %lu us\n", elapsed);
+
     for (unsigned int i = 0; i < sizeof(key); i++) {
         printf("%.2x ", ((uint8_t *) key)[i]);
     }
