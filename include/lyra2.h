43e02f563314f179b1221921034c145887a968fa
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2014-10-12 19:11:00 -0300

    Use 256 cols when in PHS mode.

--- include/lyra2.h
+++ include/lyra2.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define PHS_NCOLS 64
+#define PHS_NCOLS 256
 
 int lyra2(char *key, uint32_t keylen, const char *pwd, uint32_t pwdlen, const char *salt, uint32_t saltlen, uint32_t R, uint32_t C, uint32_t T);
 int PHS(void *out, size_t outlen, const void *in, size_t inlen, const void *salt, size_t saltlen, unsigned int t_cost, unsigned int m_cost);
